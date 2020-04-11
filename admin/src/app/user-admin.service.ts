ee60b7d3e476c46f0a63fae595739d8de6eff89f
“rikusv” <rikusv@gmail.com>
2020-04-11 16:12:18 +0200

    Update user-admin.service.ts

--- admin/src/app/user-admin.service.ts
+++ admin/src/app/user-admin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { AngularFirestore } from '@angular/fire/firestore'
-import { Observable, from } from 'rxjs'
+import { Observable } from 'rxjs'
 import 'firebase/firestore'
 
 import { User } from './user'
